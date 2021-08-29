670a2bb33b0767c83b6bf32c29388fdeba68601d
rmclough <57398808+rmclough@users.noreply.github.com>
2021-08-29 14:26:52 +0100

    Add files via upload

---
+++ Lab 2 Q2/Lab 2 Q2/Employee.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_2_Q2
+{
+    public abstract class Employee
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int PhoneNumber { get; set; }
+        public string Position { get; set; }
+        
+
+        public Employee() { }
+
+        public Employee(string name, string address, int phoneNumber, string position)
+        {
+            this.Name = name;
+            this.Address = address;
+            this.PhoneNumber = phoneNumber;
+            this.Position = position;
+            
+        }
+
+        public abstract void DisplayGrossPay();
+        
+
+        
+
+        public abstract void DisplayNetPay();
+        
+
+        
+
+        public virtual void DisplayWorkerDetails()
+        {
+            Console.WriteLine("Name: {0}\nAddress: {1}\nPhone Number: {2}\nPosition: {3}", this.Name, this.Address, this.PhoneNumber, this.Position);
+        }
+
+        public abstract void DisplayHolidayEntitlements();
+
+
+
+
+        public abstract void DisplayPensionContributions();
+        
+
+        
+
+    }
+}
