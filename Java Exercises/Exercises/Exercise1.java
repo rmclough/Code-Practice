4fdd7671599d977144d77979a685c453d344c2f6
rmclough <57398808+rmclough@users.noreply.github.com>
2021-09-17 17:57:47 +0100

    Update Exercise1.java

--- Java Exercises/Exercises/Exercise1.java
+++ Java Exercises/Exercises/Exercise1.java
@@ -15,6 +15,8 @@ public class Exercise1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        //Creating Boolean Conditions
+        
         // x > 0 && x % 2 == 1
         // x > 0 && x % 10 == 0
         // x < 0 && x % 5 == 1
